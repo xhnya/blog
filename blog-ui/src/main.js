0987ea0ba8dc1c0475c712e04cefc9a2813c5724
xhn <930957853@qq.com>
2022-03-02 16:30:38 +0800

    引入ant-design-vue

--- blog-ui/src/main.js
+++ blog-ui/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入ant-design-vue
+import { DatePicker } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).mount('#app')
+
+const app=createApp(App);
+app.use(DatePicker)
+app.use(store).use(router).mount('#app')
